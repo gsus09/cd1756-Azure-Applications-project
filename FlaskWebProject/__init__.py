975ddfaccbdc01b1002d24c85a0350ae3d7d05d3
Martin Thomas <martin.thomas@cariad.technology>
2025-10-15 16:03:59 +0200

    added  logging and fixed open todos

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,13 +7,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+import FlaskWebProject.views
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+# logging levels and handlers with app.logger
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
-
-import FlaskWebProject.views
