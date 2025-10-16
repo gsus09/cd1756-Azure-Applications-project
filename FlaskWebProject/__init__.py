fef1a7f5c83e6a77b18bffafeb1ea16c540eef6a
Martin Thomas <martin.thomas@cariad.technology>
2025-10-16 13:08:57 +0200

    move import back to button

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,7 +7,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-import FlaskWebProject.views
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,3 +20,5 @@ Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+
+import FlaskWebProject.views
